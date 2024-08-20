f08b675f6200647e1b0382002c59eaf4c42495fb
massitd <82796634+massitd@users.noreply.github.com>
2024-08-20 15:48:47 -0400

    Create partially functioning calculator.
    To do: add in the rest of the buttons.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,78 @@
-const display = document.getElementById("display");
+const display = document.querySelector("#display");
 
-const button1 = document.getElementById("one");
+let activeNumber = 0;
+let lastNumber = 0;
+let operator = "";
 
+function operate(x, a, b) {
+    if (x == "add") {
+       return add(a, b);
+    };
+    if (x == "subtract") {
+        return subtract(a, b);
+    };
+    if (x == "multiply") {
+        return multiply(a, b);
+    };
+    if (x == "divide") {
+        return divide(a, b);
+    };
+};
+
+display.textContent = activeNumber;
+
+// operator functions
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+// number buttons
+
+let button1 = document.getElementById("one");
 button1.addEventListener("click", () => {
-    button1.style.backgroundColor = "#000000";
+    lastNumber = activeNumber;
+    activeNumber = 1;
+    display.textContent = activeNumber;
+})
+
+let button2 = document.getElementById("two");
+button2.addEventListener("click", () => {
+    lastNumber = activeNumber;
+    activeNumber = 2;
+    display.textContent = activeNumber;
+})
+
+let button3 = document.getElementById("three");
+button3.addEventListener("click", () => {
+    lastNumber = activeNumber;
+    activeNumber = 3;
+    display.textContent = activeNumber;
+})
+
+// operator buttons
+
+let multiplyOperator = document.getElementById("multiply");
+multiplyOperator.addEventListener("click", () => {
+    operator = "multiply";
+});
+
+// equals button
+
+let equals = document.getElementById("equal");
+equals.addEventListener("click", () => {
+    let result = operate(operator, lastNumber, activeNumber);
+    activeNumber = result;
+    display.textContent = activeNumber;
 });
